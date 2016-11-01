a4651ab1b407d0a069d35ff422c0134e264b6564
Hubert Zając <hubert.zajac@outlook.com>
2016-11-02 00:37:49 +0100

    narrowed to square in a city and found roads

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,15 @@
 
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+import se.kodapan.osm.domain.OsmObject;
+import se.kodapan.osm.domain.root.PojoRoot;
+import se.kodapan.osm.domain.root.indexed.IndexedRoot;
+import se.kodapan.osm.domain.root.indexed.IndexedRootImpl;
 import se.kodapan.osm.parser.xml.OsmXmlParserException;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by Hubert on 01.11.2016.
@@ -10,8 +18,21 @@ public class App {
 
     public static void main(String[] args) throws IOException, OsmXmlParserException {
         GraphFactory gf = new GraphFactory();
+        IndexedRoot<Query> index = gf.makeGraph("andorra-latest.osm");
 
-        gf.makeGraph("andorra-latest.osm");
+        BooleanQuery bq = new BooleanQuery();
+
+        bq.add(index.getQueryFactories().containsTagKeyQueryFactory().setKey("highway").build(), BooleanClause.Occur.MUST);
+        bq.add(index.getQueryFactories().nodeEnvelopeQueryFactory()
+                .setSouthLatitude(42.5110129).setWestLongitude(1.527749)
+                .setNorthLatitude(42.5209083).setEastLongitude(1.544432)
+                .build(), BooleanClause.Occur.MUST);
+
+        Map<OsmObject, Float> hits = index.search(bq);
+
+        for(OsmObject entry : hits.keySet()){
+            System.out.println(" ["+  entry.getId() + "] "+ entry.getTags() + " " + entry.getAttributes());
+        }
 
     }
 
--- src/main/java/GraphFactory.java
+++ src/main/java/GraphFactory.java
@@ -1,3 +1,4 @@
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Query;
 import se.kodapan.osm.domain.root.PojoRoot;
 import se.kodapan.osm.domain.root.indexed.IndexedRoot;
@@ -6,13 +7,15 @@ import se.kodapan.osm.parser.xml.instantiated.InstantiatedOsmXmlParser;
 import sun.rmi.runtime.Log;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Hubert on 01.11.2016.
  */
 public class GraphFactory {
 
-    public void makeGraph(String mapsName) throws IOException, OsmXmlParserException {
+    public IndexedRoot<Query> makeGraph(String mapsName) throws IOException, OsmXmlParserException {
         PojoRoot root = new PojoRoot();
         IndexedRoot<Query> index = IndexedRoot.newInstance(root);
         index.open();
@@ -23,5 +26,6 @@ public class GraphFactory {
         index.commit();
 
         System.out.print(root.getNodes().size());
+        return index;
     }
 }
